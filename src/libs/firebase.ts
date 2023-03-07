bf5ee561eb5024ba2dd75f770c83278fa1525cf3
RafaelSRV <rafael-srv@hotmail.com>
2023-03-07 10:51:38 -0300

    Configurando firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getStorage} from 'firebase/storage'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGvur2O-PGsHy-n2gyQ0lNGAnc8ThWj2k",
+  authDomain: "gallery-react-rsaraiva.firebaseapp.com",
+  projectId: "gallery-react-rsaraiva",
+  storageBucket: "gallery-react-rsaraiva.appspot.com",
+  messagingSenderId: "515013476756",
+  appId: "1:515013476756:web:e038d0b81d2f6092210f81"
+};
+
+const firebaseAPP = initializeApp(firebaseConfig);
+
+export const storage = getStorage(firebaseAPP);
