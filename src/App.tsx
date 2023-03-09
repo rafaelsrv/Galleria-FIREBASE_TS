9ffa2f35748c2862810e482122b289897f2b38d3
RafaelSRV <rafael-srv@hotmail.com>
2023-03-09 14:03:10 -0300

    Conclusão de projeto

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () =>{
       setLoading(true);
       setPhotos(await Photos.getAll())
       setLoading(false);
+
     }
     
   const handleFormSubmit = async ( e: FormEvent<HTMLFormElement>) =>{
@@ -26,10 +27,20 @@ const App = () =>{
     const file = formData.get('image') as File;
     if(file && file.size > 0){
       setUploading(true);
-      setLoading(false);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
     }
 
-  }
+  
 
   return(
   <C.Container>
@@ -39,6 +50,7 @@ const App = () =>{
       <C.UploadForm method ="POST" onSubmit={handleFormSubmit}>
         <input type="file" name="image" />
         <input type="submit" value="Enviar" />
+        {uploading && "Enviando..." }
       </C.UploadForm>
       
 
--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,6 +1,8 @@
 import { Photo } from "../types/Photo";
+import {v4 as createId} from 'uuid'
 import { storage} from '../libs/firebase'
-import { ref, listAll, getDownloadURL} from 'firebase/storage'
+import { ref, listAll, getDownloadURL, uploadBytes} from 'firebase/storage'
+
 
 export const getAll = async () => {
     let list: Photo[] = [];
@@ -18,4 +20,23 @@ export const getAll = async () => {
     }
 
     return list;
+}
+
+export const insert = async (file: File) =>{
+    if( ['image/jpeg', 'image/jpg', 'image/png'].includes(file.type) ) {
+
+        let randomName = createId();
+        let newFile = ref(storage, `images/${randomName}`)
+
+        let upload = await uploadBytes(newFile, file);
+        let photoUrl = await getDownloadURL(upload.ref);
+
+        return {
+            name: upload.ref.name,
+            url: photoUrl 
+        } as Photo;
+
+    } else{
+        return new Error('Tipo de arquivo não permitido.')
+    }
 }
