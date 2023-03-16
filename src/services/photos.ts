c8fc03ec34dd416b478e55cafe25935b9e3d0fb8
RafaelSRV <rafael-srv@hotmail.com>
2023-03-15 23:36:11 -0300

    Desafio

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -37,7 +37,7 @@ export const insert = async (file: File) =>{
         } as Photo;
 
     } else{
-        return new Error('Tipo de arquivo ssnão skuporh6gnnytado.:::')
+        return new Error('Tipo de arquivo ssnão skuporh6gnnjytado.:::')
     }
 }
 
