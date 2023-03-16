0ea2e09f94f3e157ee9be09f1fadb6ca70cae9c6
RafaelSRV <rafael-srv@hotmail.com>
2023-03-15 23:35:31 -0300

    Desafio

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -37,7 +37,7 @@ export const insert = async (file: File) =>{
         } as Photo;
 
     } else{
-        return new Error('Tipo de arquivo não suportado.:::')
+        return new Error('Tipo de arquivo snão suportado.:::')
     }
 }
 
