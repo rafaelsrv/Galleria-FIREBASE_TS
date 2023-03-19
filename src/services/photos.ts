805b8a23b16c97be9f544845773096271bdb2c30
Rafael <rafael-srv@hotmail.com>
2023-03-19 10:02:05 -0300

    subindo de novo

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -37,7 +37,7 @@ export const insert = async (file: File) =>{
         } as Photo;
 
     } else{
-        return new Error('Tipo de arfqcxzuiv2o ssnbchfgvxvbxtgwecvãocxzrsrs skuporh6gnnjy6tado.:::')
+        return new Error('Tipo de arfqcxzuiv2o ssnbchfgvxvbjxtgwecvãocxzrsrs skuporh6gnnjy6tado.:::')
     }
 }
 
